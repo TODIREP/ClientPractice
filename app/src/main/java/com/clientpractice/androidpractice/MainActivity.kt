41e0273b5ce3d6bc2d7d2a7585ae8f1afdfac46d
TODIREP <56482189+TODIREP@users.noreply.github.com>
2021-04-01 15:21:48 +0900

    Data Binding 기초 구성

--- app/src/main/java/com/clientpractice/androidpractice/MainActivity.kt
+++ app/src/main/java/com/clientpractice/androidpractice/MainActivity.kt
@@ -2,10 +2,14 @@ package com.clientpractice.androidpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.clientpractice.androidpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.user = User("Kotlin", "Android")
     }
 }
